ebb4f232ec5e644272e760fdc5dc711a6292ec29
mylenavendramini <myvendramini@gmail.com>
2022-09-19 23:24:35 +0100

    interfaces

---
+++ pages/api/interfaces.ts
@@ -0,0 +1,29 @@
+export type Employee = {
+  id: string;
+  name: string;
+  height: string;
+  mass: string;
+  hair_color: string;
+  skin_color: string;
+  eye_color: string;
+  gender: string;
+};
+
+export type Recipe = {
+  id: string;
+  name: string;
+  height: string;
+  mass: string;
+  hair_color: string;
+  skin_color: string;
+  eye_color: string;
+  gender: string;
+};
+
+export type ResponseError = {
+  message: string;
+};
+
+export type Data = {
+  text: string;
+};
